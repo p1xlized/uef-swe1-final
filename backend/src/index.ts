0b987bbee6fbd11b0174264fc456c48af9ca9005
Luukas Paukkonen <luukaspa@student.uef.fi>
2026-04-10 08:18:37 +0300

    add - minor: express catch-all route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 dotenv.config();
-import express from 'express';
+import express, {Request, Response} from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import helmet from 'helmet';
@@ -11,6 +11,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 app.use(helmet());
+
+app.all('/{*splat}', async (req: Request, res: Response) => {
+    res.status(404).send({
+        message: "The route or resource you were looking for does not exist or could not be found",
+        time: new Date().toISOString(),
+        status: 'not_found',
+    });
+});
 
 app.listen(process.env.PORT || 3306, () => {
     console.error(`Fuck you. It's alive`);
