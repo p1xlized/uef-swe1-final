ab69f9f287add1ebca1d9bc42b7b4900e16a60dc
p1xlized <apaduret@pixlized.net>
2026-05-17 23:16:47 +0300

    deploy ready backend

--- frontend/src/lib/auth/better-auth.ts
+++
@@ -1,6 +0,0 @@
-import { createAuthClient } from "better-auth/react";
-
-export const authClient = createAuthClient({
-  // Point this to your Elysia server
-  baseURL: "http://localhost:3000",
-});
--- frontend/src/routes/parents/schedule.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/parents/schedule')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/parents/schedule"!</div>
-}
